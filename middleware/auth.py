8f9cbaa4fae57d07fbc1a02321b6a35df1d50981
Your Name <email@example.com>
2022-08-04 19:09:07 +0800

    zzl

--- middleware/auth.py
+++ middleware/auth.py
@@ -6,6 +6,9 @@ import datetime
 
 class update_expired(MiddlewareMixin):
     def process_request(self, request):
+        widget = ['/login/', '/logout/', '/signup/', '/']
+        if request.path_info in widget:
+            return
         now = datetime.datetime.now()
         today = datetime.date.today()
         Todo.objects.filter(user=request.user, expiration_date__lt=now, datecompleted__isnull=True,
